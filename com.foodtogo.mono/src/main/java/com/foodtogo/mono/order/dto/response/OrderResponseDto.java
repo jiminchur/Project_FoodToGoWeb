786fb60277f8c9f96e9c9bf576938b322e4d17ab
atto08 <atto08@naver.com>
2024-08-27 21:08:24 +0900

    [Init] 주문 상태 변경 구현

---
+++ com.foodtogo.mono/src/main/java/com/foodtogo/mono/order/dto/response/OrderResponseDto.java
@@ -0,0 +1,38 @@
+package com.foodtogo.mono.order.dto.response;
+
+import com.foodtogo.mono.order.core.domain.Order;
+import com.foodtogo.mono.order.core.enums.OrderStatusEnum;
+import com.foodtogo.mono.order.core.enums.OrderTypeEnum;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderResponseDto {
+    private UUID orderId;
+    private UUID userId;
+    private UUID restaurantId;
+    private OrderTypeEnum orderType;
+    private OrderStatusEnum orderStatus;
+    private BigDecimal amount;
+    private Boolean isPaid;
+    private List<OrderFoodResponseDto> orderFoodList;
+
+    public OrderResponseDto(Order order) {
+        this.orderId = order.getOrderId();
+        this.userId = order.getUser().getUserId();
+        this.restaurantId = order.getRestaurant().getRestaurantId();
+        this.orderType = order.getOrderType();
+        this.orderStatus = order.getOrderStatus();
+        this.amount = order.getAmount();
+        this.isPaid = order.getIsPaid();
+    }
+}
