3cf3309e76e2dca3af52cbf69c4ad1cfa05eea95
jiminchur <ttlcc13@naver.com>
2024-08-29 11:43:08 +0900

    [Add] ai 요청 및 요청,답변기록 데이터베이스에 저장 기능 구현

---
+++ com.foodtogo.mono/src/main/java/com/foodtogo/mono/ai/core/AiRequestHistory.java
@@ -0,0 +1,47 @@
+package com.foodtogo.mono.ai.core;
+
+import com.foodtogo.mono.log.LogEntity;
+import com.foodtogo.mono.user.core.domain.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "p_ai_request_history")
+public class AiRequestHistory extends LogEntity {
+
+    @Id
+    @UuidGenerator
+    private UUID aiRequestHistoryId;
+
+    // 음식점 소유자 ID, FK
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false )
+    private User user;
+
+    @Column(columnDefinition = "TEXT")
+    private String aiRequest;
+
+    @Column(columnDefinition = "TEXT")
+    private String aiResponse;
+
+    public AiRequestHistory(
+        User user,
+        String aiRequest,
+        String aiResponse,
+        String createdBy
+    ){
+        this.aiRequest = aiRequest;
+        this.aiResponse = aiResponse;
+        this.user = user;
+
+        setCreatedBy(createdBy);
+    }
+}
