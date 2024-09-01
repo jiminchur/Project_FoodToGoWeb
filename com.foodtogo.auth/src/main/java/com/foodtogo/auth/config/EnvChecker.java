d64886d2ad234a6ddda56456bf0a51942535b03c
jiminchur <ttlcc13@naver.com>
2024-09-02 01:15:17 +0900

    [Add] application.yml Github Sercrets 사용을 위한 수정 & [Add] Dockerfile 모든 서비스 폴더 안에 추가 & [Remove] test 주석처리 및 ip주소 수정

--- com.foodtogo.auth/src/main/java/com/foodtogo/auth/config/EnvChecker.java
+++ com.foodtogo.auth/src/main/java/com/foodtogo/auth/config/EnvChecker.java
@@ -1,61 +1,61 @@
-package com.foodtogo.auth.config;
-
-import jakarta.annotation.PostConstruct;
-import lombok.Getter;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
-@Slf4j
-@Getter
-@Component
-public class EnvChecker {
-
-	@Value("${DB_URL}")
-	private String dbUrl;
-
-	@Value("${DB_USERNAME}")
-	private String dbUsername;
-
-	@Value("${DB_PASSWORD}")
-	private String dbPassword;
-
-	@Value("${REDIS_PASSWORD}")
-	private String redisPassword;
-
-
-	@Value("${spring.datasource.url}")
-	private String dbUrlFromApplication;
-
-	@Value("${spring.datasource.username}")
-	private String dbUsernameFromApplication;
-
-	@Value("${spring.datasource.password}")
-	private String dbPasswordFromApplication;
-
-	@Value("${spring.data.redis.password}")
-	private String redisPasswordFromApplication;
-
-
-	@PostConstruct
-	public void checkEnv() {
-
-		// spring run: vm option 확인
-		System.getProperty("DB_URL");
-		System.getProperty("DB_USERNAME");
-		System.getProperty("DB_PASSWORD");
-		System.getProperty("REDIS_PASSWORD");
-
-		log.debug("[option] DB_URL: " + dbUrl);
-		log.debug("[option] DB_USERNAME: " + dbUsername);
-		log.debug("[option] DB_PASSWORD: " + dbPassword);
-		log.debug("[option] REDIS_PASSWORD: " + redisPassword);
-
-		// application.yml에 직접 주입한 값 확인
-		log.debug("[yml] DB_URL: " + dbUrlFromApplication);
-		log.debug("[yml] DB_USERNAME: " + dbUsernameFromApplication);
-		log.debug("[yml] DB_PASSWORD: " + dbPasswordFromApplication);
-		log.debug("[yml] REDIS_PASSWORD: " + redisPasswordFromApplication);
-
-	}
-}
+//package com.foodtogo.auth.config;
+//
+//import jakarta.annotation.PostConstruct;
+//import lombok.Getter;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Component;
+//
+//@Slf4j
+//@Getter
+//@Component
+//public class EnvChecker {
+//
+//	@Value("${DB_URL}")
+//	private String dbUrl;
+//
+//	@Value("${DB_USERNAME}")
+//	private String dbUsername;
+//
+//	@Value("${DB_PASSWORD}")
+//	private String dbPassword;
+//
+//	@Value("${REDIS_PASSWORD}")
+//	private String redisPassword;
+//
+//
+//	@Value("${spring.datasource.url}")
+//	private String dbUrlFromApplication;
+//
+//	@Value("${spring.datasource.username}")
+//	private String dbUsernameFromApplication;
+//
+//	@Value("${spring.datasource.password}")
+//	private String dbPasswordFromApplication;
+//
+//	@Value("${spring.data.redis.password}")
+//	private String redisPasswordFromApplication;
+//
+//
+//	@PostConstruct
+//	public void checkEnv() {
+//
+//		// spring run: vm option 확인
+//		System.getProperty("DB_URL");
+//		System.getProperty("DB_USERNAME");
+//		System.getProperty("DB_PASSWORD");
+//		System.getProperty("REDIS_PASSWORD");
+//
+//		log.debug("[option] DB_URL: " + dbUrl);
+//		log.debug("[option] DB_USERNAME: " + dbUsername);
+//		log.debug("[option] DB_PASSWORD: " + dbPassword);
+//		log.debug("[option] REDIS_PASSWORD: " + redisPassword);
+//
+//		// application.yml에 직접 주입한 값 확인
+//		log.debug("[yml] DB_URL: " + dbUrlFromApplication);
+//		log.debug("[yml] DB_USERNAME: " + dbUsernameFromApplication);
+//		log.debug("[yml] DB_PASSWORD: " + dbPasswordFromApplication);
+//		log.debug("[yml] REDIS_PASSWORD: " + redisPasswordFromApplication);
+//
+//	}
+//}
--- com.foodtogo.auth/src/test/java/com/foodtogo/config/EnvCheckerTest.java
+++ com.foodtogo.auth/src/test/java/com/foodtogo/config/EnvCheckerTest.java
@@ -1,44 +1,44 @@
-package com.foodtogo.config;
-
-import com.foodtogo.auth.config.EnvChecker;
-import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-
-@Slf4j
-@SpringBootTest(classes = EnvChecker.class)
-@ActiveProfiles("dev")
-public class EnvCheckerTest {
-
-	@Autowired
-	private EnvChecker envChecker;
-
-	@Test
-	public void testEnvProperties() {
-		// Validate system properties
-		String expectedDbUrl = System.getProperty("DB_URL");
-		String expectedDbUsername = System.getProperty("DB_USERNAME");
-		String expectedDbPassword = System.getProperty("DB_PASSWORD");
-		String expectedRedisPassword = System.getProperty("REDIS_PASSWORD");
-
-		// Validate application.yml properties
-		String dbUrlFromApplication = envChecker.getDbUrlFromApplication();
-		String dbUsernameFromApplication = envChecker.getDbUsernameFromApplication();
-		String dbPasswordFromApplication = envChecker.getDbPasswordFromApplication();
-		String redisPasswordFromApplication = envChecker.getRedisPasswordFromApplication();
-
-
-		log.info("[option] DB_URL: " + expectedDbUrl);
-		log.info("[option] DB_USERNAME: " + expectedDbUsername);
-		log.info("[option] DB_PASSWORD: " + expectedDbPassword);
-		log.info("[option] REDIS_PASSWORD: " + expectedRedisPassword);
-
-		// application.yml에 직접 주입한 값 확인
-		log.info("[yml] DB_URL: " + dbUrlFromApplication);
-		log.info("[yml] DB_USERNAME: " + dbUsernameFromApplication);
-		log.info("[yml] DB_PASSWORD: " + dbPasswordFromApplication);
-		log.info("[yml] REDIS_PASSWORD: " + redisPasswordFromApplication);
-	}
-}
+//package com.foodtogo.config;
+//
+//import com.foodtogo.auth.config.EnvChecker;
+//import lombok.extern.slf4j.Slf4j;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.ActiveProfiles;
+//
+//@Slf4j
+//@SpringBootTest(classes = EnvChecker.class)
+//@ActiveProfiles("dev")
+//public class EnvCheckerTest {
+//
+//	@Autowired
+//	private EnvChecker envChecker;
+//
+//	@Test
+//	public void testEnvProperties() {
+//		// Validate system properties
+//		String expectedDbUrl = System.getProperty("DB_URL");
+//		String expectedDbUsername = System.getProperty("DB_USERNAME");
+//		String expectedDbPassword = System.getProperty("DB_PASSWORD");
+//		String expectedRedisPassword = System.getProperty("REDIS_PASSWORD");
+//
+//		// Validate application.yml properties
+//		String dbUrlFromApplication = envChecker.getDbUrlFromApplication();
+//		String dbUsernameFromApplication = envChecker.getDbUsernameFromApplication();
+//		String dbPasswordFromApplication = envChecker.getDbPasswordFromApplication();
+//		String redisPasswordFromApplication = envChecker.getRedisPasswordFromApplication();
+//
+//
+//		log.info("[option] DB_URL: " + expectedDbUrl);
+//		log.info("[option] DB_USERNAME: " + expectedDbUsername);
+//		log.info("[option] DB_PASSWORD: " + expectedDbPassword);
+//		log.info("[option] REDIS_PASSWORD: " + expectedRedisPassword);
+//
+//		// application.yml에 직접 주입한 값 확인
+//		log.info("[yml] DB_URL: " + dbUrlFromApplication);
+//		log.info("[yml] DB_USERNAME: " + dbUsernameFromApplication);
+//		log.info("[yml] DB_PASSWORD: " + dbPasswordFromApplication);
+//		log.info("[yml] REDIS_PASSWORD: " + redisPasswordFromApplication);
+//	}
+//}
